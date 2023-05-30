2888dbaa931625256dc8114a4bc4bcac8bb8615e
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-30 18:40:44 +0600

    BISTRO BOSS - Restaurant - ProjectCreated

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import useTitleName from "../../../Hooks/useTitleName";
+
+
+const Home = () => {
+    useTitleName('Home Page')
+    return (
+        <div>
+            <h3>HOme</h3>
+        </div>
+    );
+};
+
+export default Home;
